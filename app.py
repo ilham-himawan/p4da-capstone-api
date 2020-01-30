51fca896141bc3574d7e08927a187dd624deab4a
iqbalbasyar <the.underground.eleven@gmail.com>
2020-01-30 21:20:44 +0700

    test commit

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 from flask import Flask, request 
 import pandas as pd 
-import os
-
 app = Flask(__name__) 
-
-@app.route('/')
-def index():
-    return "<h1>Welcome to our Capstone Server!!</h1>"
 
 # mendapatkan keseluruhan data dari <data_name>
 @app.route('/data/get/<data_name>', methods=['GET']) 
 def get_data(data_name): 
     data = pd.read_csv('data/' + str(data_name))
     return (data.to_json())
-    
+ 
 
 # mendapatkan data dengan filter nilai <value> pada kolom <column>
 @app.route('/data/get/equal/<data_name>/<column>/<value>', methods=['GET']) 
@@ -24,7 +18,4 @@ def get_data_equal(data_name, column, value):
     return (data.to_json())
 
 if __name__ == '__main__':
-    
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-#     app.run(debug=True, port=8000) 
+    app.run(debug=True, port=5000) 
